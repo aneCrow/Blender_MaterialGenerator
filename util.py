f4365a97afbc02de25ac4ef8ee524731c65761cc
aneCrow <anecrow@outlook.com>
2020-03-31 19:57:03 +0800

    new func parseNodeSockets

---
+++ util.py
@@ -0,0 +1,10 @@
+import bpy
+
+
+def parseNodeSockets(node):
+    """parse material node inputs & outputs"""
+    sockets_dict = {
+        "inputs": [],
+        "outputs": []
+    }
+    return sockets_dict
